3ea7807b0eeaecc9581a847e4b3d6667eb0c1d67
Michelle Oliveira <miolivc@outlook.com>
2017-07-18 17:44:06 -0300

    Utilizando funcao para retornar apenas a noticia

--- app/views.py
+++ app/views.py
@@ -10,76 +10,38 @@ def query(tagBox):
 	conn = sqlite3.connect("noticias.db")
 	cursor = conn.cursor()
 	if tagBox != None:
-		cursor.execute("SELECT * FROM NOTICIAS WHERE TAGBOX = ?", tagBox)
+		cursor.execute("SELECT * FROM NOTICIAS WHERE TAGBOX = ?", [tagBox])
 	else:
 		cursor.execute("SELECT * FROM NOTICIAS")
 	noticia = choice(cursor.fetchall())
 	conn.close()
 	return noticia
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def index():
 	return "<h1>Você não escolheu uma tag!</h1>"
 
 @app.route('/nutricao')
 def nutricao():
 	noticia = query("nutricao")
-	return render_template('index.html', tagBox=noticia[1], imgBox=noticia[2], titulo=noticia[3],
-	 fontBox=noticia[4], descricao=noticia[5], mainBox=noticia[6])
+	return render_template('index.html', tagBox=noticia[1], imgBox=noticia[2], titulo=noticia[3], fontBox=noticia[4], descricao=noticia[5], mainBox=noticia[6])
 
 @app.route('/saude')
 def saude():
-	conn = sqlite3.connect("noticias.db")
-	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM NOTICIAS WHERE TAGBOX = 'saude'")
-	noticia = choice(cursor.fetchall())
-	tag = noticia[1]
-	img = noticia[2]
-	title = noticia[3]
-	font = noticia[4]
-	description = noticia[5]
-	text = noticia[6]
-	conn.close()
-	return render_template('index.html', tagBox=tag, imgBox=img, titulo=title, fontBox=font,descricao=description, mainBox=text)
+	noticia = query("saude")
+	return render_template('index.html', tagBox=noticia[1], imgBox=noticia[2], titulo=noticia[3], fontBox=noticia[4], descricao=noticia[5], mainBox=noticia[6])
 
 @app.route('/dieta')
 def dieta():
-	conn = sqlite3.connect("noticias.db")
-	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM NOTICIAS WHERE TAGBOX = 'dieta'")
-	noticia = choice(cursor.fetchall())
-	tag = noticia[1]
-	img = noticia[2]
-	title = noticia[3]
-	font = noticia[4]
-	description = noticia[5]
-	text = noticia[6]
-	conn.close()
-	return render_template('index.html', tagBox=tag, imgBox=img, titulo=title, fontBox=font,descricao=description, mainBox=text)
+	noticia = query("dieta")
+	return render_template('index.html', tagBox=noticia[1], imgBox=noticia[2], titulo=noticia[3], fontBox=noticia[4], descricao=noticia[5], mainBox=noticia[6])
 
 @app.route('/energiaesaude')
 def energiaesaude():
-	conn = sqlite3.connect("noticias.db")
-	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM NOTICIAS WHERE TAGBOX = 'academia'")
-	noticia = choice(cursor.fetchall())
-	img = noticia[2]
-	title = noticia[3]
-	font = noticia[4]
-	description = noticia[5]
-	text = noticia[6]
-	conn.close()
-	return render_template('energiaesaude.html', imgBox=img, titulo=title, fontBox=font, descricao=description, mainBox=text)
+	noticia = query("energiaesaude")
+	return render_template('index.html', tagBox=noticia[1], imgBox=noticia[2], titulo=noticia[3], fontBox=noticia[4], descricao=noticia[5], mainBox=noticia[6])
 
 @app.route('/belezamasculina')
 def belezamasculina():
-	conn = sqlite3.connect("noticias.db")
-	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM NOTICIAS WHERE TAGBOX = 'beleza'")
-	noticia = choice(cursor.fetchall())
-	img = noticia[2]
-	title = noticia[3]
-	font = noticia[4]
-	text = noticia[6]
-	conn.close()
-	return render_template('belezamasculina.html', imgBox=img, titulo=title, fontBox=font, mainBox=text)
+	noticia = query("belezamasculina")
+	return render_template('index.html', tagBox=noticia[1], imgBox=noticia[2], titulo=noticia[3], fontBox=noticia[4], descricao=noticia[5], mainBox=noticia[6])
